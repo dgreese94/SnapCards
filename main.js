ee84dafc3ac7eb70c9b9553983b390e1b839684e
dylan_reese <dgr3547@rit.edu>
2016-10-31 22:43:34 -0400

    Added home page.

--- main.js
+++ main.js
@@ -1,31 +1,66 @@
 var snapCards = angular.module('snapCards', ['ngMaterial', 'ngRoute']);
- 
-snapCards.config(function($routeProvider) {
-        $routeProvider
 
-            // // route for the home page
-            // .when('/', {
-            //     templateUrl : 'pages/home.html',
-            //     controller  : 'homeController'
-            // })
+snapCards.config(function($routeProvider, $mdThemingProvider) {
+    $routeProvider
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+    // route for the home page
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
 
-            // // route for the contact page
-            // .when('/contact', {
-            //     templateUrl : 'pages/contact.html',
-            //     controller  : 'contactController'
-            // });
+    // route for the about page
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
     });
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('amber')
+        .accentPalette('brown');
+});
 
 snapCards.controller('AppController', function() {
     this.user = {
         name: 'Studious Sally'
     };
-  });
+});
 
 snapCards.controller('aboutController', function() {});
+snapCards.controller('homeController', function($scope) {
+    $scope.MyDecks = [{
+        name: 'Presidents',
+        description: 'The presidents of the United States of America.',
+        cards: [{}, {}, {}, {}, {}]
+    }, {
+        name: 'Geometry',
+        description: 'Name the shape.',
+        cards: [{}, {}, {}, {}, {}]
+    }];
+
+    $scope.PublicDecks = [{
+        name: 'Presidents',
+        description: 'The presidents of the United States of America.',
+        cards: [{}, {}, {}, {}, {}]
+    }, {
+        name: 'Periodic Table',
+        description: 'Name the Element.',
+        cards: [{}, {}, {}, {}, {}]
+    }];
+
+    $scope.edit = function edit(deck) {
+        alert('Edit '+deck.name);
+    };
+
+    $scope.share = function share(deck) {
+        alert('Share '+deck.name);
+    };
+
+    $scope.study = function study(deck) {
+        alert('Study '+deck.name);
+    };
+
+    $scope.addNewDeck = function addNewDeck() {
+        alert('Add New Deck pressed');
+    };
+});
