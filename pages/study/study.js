86d51448b137e547f65d8bdefb9f301eae836fcf
Ben Grawi <Ben Grawi>
2016-11-07 01:32:18 -0500

    Got Study page finished and fixed some layout issues

--- pages/study/study.js
+++ pages/study/study.js
@@ -1,5 +1,22 @@
 angular.module('snapCards.study', ['ngMaterial', 'ngRoute', 'snapCards.deckservice'])
-.controller('studyController', function($scope, $routeParams, snapAPI) {
-    $scope.deck = snapAPI.getDeck(parseInt($routeParams.id, 10));
-    $scope.currentCard = 0;
-});
+    .controller('studyController', function ($scope, $routeParams, snapAPI) {
+        $scope.deck = snapAPI.getDeck(parseInt($routeParams.id, 10));
+        $scope.currentCard = 0;
+        $scope.isFlipped = false;
+        $scope.maxCard = $scope.deck.cards.length - 1;
+
+
+        $scope.prevCard = function () {
+            $scope.currentCard--;
+            $scope.isFlipped = false;
+        };
+
+        $scope.nextCard = function () {
+            $scope.currentCard++;
+            $scope.isFlipped = false;
+        };
+
+        $scope.flip = function () {
+            $scope.isFlipped = !$scope.isFlipped;
+        }
+    });
